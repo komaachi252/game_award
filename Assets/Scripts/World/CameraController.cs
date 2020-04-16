bdd2d2c8136641a0eca99d6a60df453ea43d001b
hakiyoshi <harusyu0306@gmail.com>
2020-04-16 20:47:46 +0900

    拡張しやすいように調整 選択されたワールドの値を渡すsetterを作成

--- Assets/Scripts/World/CameraController.cs
+++ Assets/Scripts/World/CameraController.cs
@@ -5,54 +5,99 @@ using UnityEngine;
 public class CameraController: MonoBehaviour
 {
 
-    private const float EARTH_RANGE = 6.0f;//地球とカメラの距離
-    private const int START_WORLD = 1;//最初のワールド
-    private const int MOVE_FLAME = 50;//移動フレーム数
+    private const float WORLD_EARTH_RANGE = 6.0f;//地球とカメラの距離　ワールドバージョン
+    private const float STAGE_EARTH_RANGE = 4.0f;//地球とカメラの距離　ステージバージョン
+    private const int MOVE_FLAME = 20;//移動フレーム数
 
-    public GameObject earth;//地球
-    public GameObject[] marking;//マーキングオブジェクト
+    public Transform earth;//地球
 
-    private int now_world = START_WORLD;//今のワールド
-    private int next_world = START_WORLD;//次のワールド
+    private Transform[] world_marking;//ワールドマーキング
 
-    float flame;//一回で移動する速度
+    private StageController stagecontroller;//ステージコントローラーの情報入れる
+
+    private float flame;//一回で移動する速度
+
+    private Vector3 Front_vec;//目標までのベクトル
+
+    private float earth_range;//地球との距離
 
     // Start is called before the first frame update
     void Start()
     {
 
-        ChangeWorld(marking[START_WORLD].transform.position, 12);
+        //====================================
+
+        GameObject stagecon = GameObject.Find("StageController");//ステージコントローラーオブジェをもらう
+        stagecontroller = stagecon.GetComponent<StageController>();//ステージコントローラーのスクリプトをもらう
+
+        //====================================
+
+        GameObject worldmarking = GameObject.Find("WorldMarking");//ワールドマーキングが入ってるやつ貰う
+        int world_child_num = 0;//子供の数
+        foreach (Transform tr in worldmarking.transform)//数を数える
+        {
+            world_child_num++;
+        }
+
+        world_marking = new Transform[world_child_num];//数を確定
+
+        for (int i = 0; i < world_child_num; i++)//子供の情報をもらう
+        {
+            world_marking[i] = worldmarking.transform.GetChild(i).gameObject.transform;
+        }
+
+        //====================================
+
+        if (stagecontroller.Get_SelectFlag() == 0)//ワールド選択の場合
+        {
+            earth_range = WORLD_EARTH_RANGE;
+        }
+        else if (stagecontroller.Get_SelectFlag() == 1)//ステージ選択の場合
+        {
+            earth_range = STAGE_EARTH_RANGE;
+        }
+
+        //====================================
+
+        ChangeWorld(world_marking[StageController.START_WORLD].position, 12);//初期位置に移動
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A) && next_world != 0)//ワールドを戻る
-        {
-            next_world--;
-            //Debug.Log("戻る");
-        }
+        int now_world = StageController.Get_world();//現在選択してるワールド
+        int next_world = stagecontroller.Get_nextworld();//次に選択されるワールド
+        int select_flag = stagecontroller.Get_SelectFlag();//ワールド選択かステージ選択かフラグ
 
-        if (Input.GetKeyDown(KeyCode.Q) && next_world != marking.Length - 1)//ワールドを進む
-        {
-            next_world++;
-            //Debug.Log("進む");
-        }
+        //=========================
 
+        //ワールド選択時のカメラ
         if (now_world != next_world)//移動先がちう場合
         {
             //移動量計算
-            
+
             float kakudo;//目標までの角度
-            kakudo = Vector3.Angle(this.transform.position - earth.transform.position, marking[next_world].transform.position - earth.transform.position);
+            kakudo = Vector3.Angle(this.transform.position - earth.position, world_marking[next_world].position - earth.position);
             flame = kakudo / MOVE_FLAME;//一回の移動量
             flame = flame * Time.deltaTime;
 
-            now_world = next_world;//目標と現在の位置を同じにする
+            stagecontroller.Set_world(next_world);//現在選択してるワールドを変更する
         }
 
-        ChangeWorld(marking[next_world].transform.position, flame);//ワールド変更
-        
+        ChangeWorld(world_marking[next_world].position, flame);//ワールド移動
+
+        //=========================
+
+        if (stagecontroller.Get_SelectFlag() == 0)//ワールド選択の場合
+        {
+            earth_range = WORLD_EARTH_RANGE;
+        }
+        else if (stagecontroller.Get_SelectFlag() == 1)//ステージ選択の場合
+        {
+            earth_range = STAGE_EARTH_RANGE;
+        }
+
     }
 
     //ワールド変更
@@ -61,22 +106,18 @@ public class CameraController: MonoBehaviour
     //move = 移動量
     private void ChangeWorld(Vector3 target_posi,float move)
     {
-        Vector3 Front_vec;//目標までのベクトル
+        
 
-        this.transform.position = earth.transform.position;//地球の場所に移動
+        this.transform.position = earth.position;//地球の場所に移動
 
         Vector3 target_vec = this.transform.position - target_posi;//目標までのベクトル
+
         Quaternion rot = Quaternion.LookRotation(target_vec);//回転軸計算
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, rot, move);//回転するやつ
 
-        Front_vec = this.transform.rotation * Vector3.forward;//マーキングされているほうのベクトルを取得
+        Front_vec = this.transform.rotation * Vector3.forward;//今見てる方向のベクトルを取得
 
-        this.transform.position = -Front_vec * EARTH_RANGE;//移動
+        this.transform.position = -Front_vec * earth_range;//移動
     }
 
-    //今の選択されているワールドをもらう
-    public int Get_NowWorld
-    {
-        get { return now_world; }
-    }
 }
---
+++ Assets/Scripts/World/StageController.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageController : MonoBehaviour
+{
+    public static readonly int START_WORLD = 0;//初期ワールド　0スタート
+    public static readonly int START_STAGE = 0;//初期ステージ　0スタート
+
+    public int next_world;//次に選択されてるワールド
+    private static int now_world;//現在選択してるワールド
+
+    public int next_stage;//次に選択されているワールド
+    private static int now_stage;//現在選択してるステージ
+
+    private int world_marking_num;//ワールド数
+
+    private int one_read;//最初に一回だけ呼ばれるようにするフラグ
+
+    private int select_flag;//今ステージ選択中かワールド選択中か
+
+    //スタートより早く呼ばれるらしい
+    void Awake()
+    {
+        if (one_read == 0)//一回だけ実行
+        {
+            //初期化
+            next_world = START_WORLD;
+            now_world = START_WORLD;
+
+            next_stage = START_STAGE;
+            now_stage = START_STAGE;
+        }
+    }
+
+    //これに入ってるオブジェクトが破壊されるタイミングで呼ばれるらしい
+    void OnDestroy()
+    {
+        one_read = 1;//一回きりフラグ
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //ワールド数をカウントする
+        GameObject WorldMarking = GameObject.Find("WorldMarking");
+        foreach (Transform child in WorldMarking.transform)
+        {
+            world_marking_num++;
+        }
+
+        select_flag = 0;//ワールド選択から始める
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))//進む
+        {
+            if (select_flag == 0 && next_world < world_marking_num - 1)//ワールド選択画面
+            {
+                next_world++;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))//戻る
+        {
+            if (select_flag == 0 && next_world > 0)//ワールド選択画面
+            {
+                next_world--;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))//決定
+        {
+            if (select_flag == 0)//ワールド選択の時
+            {
+                select_flag = 1;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))//決定してたら一個前に戻る
+        {
+            if (select_flag == 1)//ステージ選択の時
+            {
+                select_flag = 0;
+            }
+        }
+    }
+
+
+    //現在選択してるワールドをあげる関数
+    public static int Get_world()
+    {
+        return now_world;
+    }
+
+
+    //現在選択してるステージをあげる関数
+    public static int Get_stage()
+    {
+        return now_stage;
+    }
+
+
+    //次に選択されるワールドをあげる関数
+    public int Get_nextworld()
+    {
+        return next_world;
+    }
+
+    //次に選択されるステージをあげる関数
+    public int Get_nextstage()
+    {
+        return next_stage;
+    }
+
+    //ワールドを選択してるかステージを選択してるか
+    public int Get_SelectFlag()
+    {
+        return select_flag;
+    }
+
+    //現在選択してるワールドの値を変更する関数
+    public void Set_world(int set)
+    {
+        now_world = set;
+    }
+
+
+}
