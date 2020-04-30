42c4d017e28b54f0f230dfa1c58b525ec3787e22
komachi114 <oreoresuke@gmail.com>
2020-04-30 13:57:02 +0900

    背景追加

---
+++ Assets/Scripts/Game/Back_Ground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Back_Ground : MonoBehaviour
+{
+
+    public Sprite[] m_images;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
