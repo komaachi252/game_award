bce022e3c8b1fdda1f5554a35df10cc0a37e135b
komachi114 <oreoresuke@gmail.com>
2020-05-11 16:53:50 +0900

    サウンド６

---
+++ Assets/Scripts/Scene_Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Manager : MonoBehaviour
+{
+    public Animator transition_anim;
+    public string scene_name;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Load_Scene());
+        }
+    }
+
+    IEnumerator Load_Scene()
+    {
+        transition_anim.SetTrigger("end");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(scene_name);
+    }
+}
