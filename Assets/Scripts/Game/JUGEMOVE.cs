e70bba6f0b99c7d7332648fa3ffe6186e4e14859
NABRL <h_yu_nabrl@yahoo.co.jp>
2020-05-15 18:14:41 +0900

    プレイヤー更新

---
+++ Assets/Scripts/Game/JUGEMOVE.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class JUGEMOVE : MonoBehaviour
+{
+    public PLAYER PLAYER;
+    RaycastHit hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        Vector3 POS = transform.position;
+        Vector3 muki = new Vector3(0.0f, 1.0f, 0.0f);
+        POS.x += 1;
+        POS.y += 0.7f;
+        */
+        //Ray ray=new Ray(POS,muki);
+        //Physics.Raycast(POS, HIT, 1.0f);
+        /*
+        if (Physics.BoxCast(POS, new Vector3(0.45f, 0.1f, 0.45f), muki, out hit, transform.rotation, 0.2f))
+        {
+            Debug.Log(hit.transform.name);
+        }
+        */
+        /*
+        if (Physics.CheckBox(POS, new Vector3(0.45f, 0.05f, 0.45f), transform.rotation))
+        {
+            Debug.Log("ある");
+        }
+        else
+        {
+            Debug.Log("ない");
+        }
+        */
+    }
+
+    public void SETposx(int x)
+    {
+        Vector3 POS = transform.position;
+
+        POS.x = x+0.5f;
+        transform.position = POS;
+        
+    }
+
+    public void SETposy(int y)
+    {
+        Vector3 POS = transform.position;
+
+        POS.y = y;
+        transform.position = POS;
+        
+    }
+
+    public int root(int MOVE_D,int MOVE_V)
+    {
+        Vector3 POS = transform.position;
+
+        if (MOVE_D == 1)
+        {
+            POS.x += 1.0f;
+        }
+        else
+        {
+            POS.x -= 1.0f;
+        }
+
+        if (MOVE_V == 1)
+        {
+            POS.y -= 0.7f;
+        }
+        else
+        {
+            POS.y += 0.7f;
+        }
+
+        if (Physics.CheckBox(POS, new Vector3(0.45f, 0.05f, 0.45f), transform.rotation))
+        {
+            Debug.Log("ある");
+            return 0;
+        }
+
+        Debug.Log("ない"+ transform.position + "POS" + POS);
+
+        PLAYER.SET_V_MOVEPOS(POS.x);
+        return 1;       //移動予定地が空白の場合縦移動予約フラグをセット
+    }
+
+    public float GET_JUGEPOS()
+    {
+        return transform.position.x;
+    }
+
+    /*void OnDrawGizmos()
+    {
+        Vector3 POS = transform.position;
+        POS.x += 1;
+        POS.y += 0.5f;
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(POS, new Vector3(0.9f,0.2f,0.9f));
+    }
+    */
+}
--- Assets/Scripts/Game/PLAYER.cs
+++ Assets/Scripts/Game/PLAYER.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PLAYER : MonoBehaviour
 {
+    //public PLAYERCAMERA PLAYERCAMERA;
     public SOLID SOLID;
     public AQUA AQUA;
     public CLOUD CLOUD;
+    public JUGEMOVE JUGEMOVE;
     PhysicMaterial PM;
+    Rigidbody rb;
     Collider boxcollider;
     Collider capcollider;
+    public int posx = 0;
+    public int posy = 0;
+    public int B_posx = 0;
+    public int B_posy = 0;
     public int TYPE = 0;
     public int MOVE_D = 1;
-    float STARTPOINT = 0;
+    public int MOVE_V = 1;
+    public int B_MOVE_D = 1;
+    public int VMOVEflag = 0;   //縦移動予約フラグ
+    public int AUTOMOVEflag = 0;    //強制移動フラグ
+    public int AUTOMOVEflag2 = 0;   //強制移動フラグ２
+    public float VMOVEPOS = 0;
+    public float MOVEPOS = 0;        //強制移動目標地点
+    public int MOVEFinish = 0;      //強制移動完了フラグ
+    public int TIME_T = 0;          //GAP通過用カウンタ
+    public int GAPMOVE = 0;         //GAP通過フラグ
+    public float TARGETV = 0;         //
+
+    //float STARTPOINT = 0;
     public int MOVE_NOW = 0;
     public int STAND = 0;       //接地フラグ
     public int STAND_T = 0;     //頭上接触フラグ
@@ -23,6 +42,9 @@ public class PLAYER : MonoBehaviour
     public int stay_COLD_R = 0; //仮COKD接触フラグ右側
     public int stay_COLD_L = 0; //仮COLD接触フラグ左側
     public int stay_COLD = 0;   //COLD完全接触フラグ
+    public int stay_GAP = 0;    //GAP接触フラグ
+    public int stay_HARDHOT = 0;//HARDHOTフラグ
+    public int stay_HARDCOLD = 0;//HARDCOLDフラグ
 
     int exchangecount = 0;  //状態変化演出カウント
 
@@ -34,6 +56,7 @@ public class PLAYER : MonoBehaviour
     void Start()
     {
         //GetComponent<Renderer>().material.color = colors[TYPE];
+        rb = this.GetComponent<Rigidbody>();
         PM = this.gameObject.GetComponent<PhysicMaterial>();
         boxcollider = this.gameObject.GetComponent<BoxCollider>();
         capcollider = this.gameObject.GetComponent<CapsuleCollider>();
@@ -43,11 +66,12 @@ public class PLAYER : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A) && MOVE_NOW == 0 && exchangecount == 0)
+        if (Input.GetKeyDown(KeyCode.Space) && MOVE_NOW == 0 && exchangecount == 0)
         {
             Debug.Log("入力");
             if (stay_HOT == 1)
             {
+                rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
                 exchangecount = 61;
                 if (TYPE < 2)
                 {
@@ -74,6 +98,7 @@ public class PLAYER : MonoBehaviour
 
             if (stay_COLD == 1)
             {
+                rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
                 exchangecount = 61;
                 if (TYPE > 0)
                 {
@@ -98,46 +123,68 @@ public class PLAYER : MonoBehaviour
                 }
             }
 
+            if (stay_GAP == 1 && TYPE == 1 && MOVE_V == 1) //水でGAPの上にいるとき
+            {
+                //capcollider.enabled = false;
+                Debug.Log(transform.position);
+                MOVEPOS = JUGEMOVE.GET_JUGEPOS();   //目標地点Xを判定マスと同じにする
+
+                if (transform.position.x > MOVEPOS)
+                {
+                    MOVE_D = -1;
+                }
+                else
+                {
+                    MOVE_D = 1;
+                }
+
+                AUTOMOVEflag2 = 1;
+                Debug.Log(transform.position.x);
+                Debug.Log(MOVEPOS);
+            }
+
             //GetComponent<Renderer>().material.color = colors[TYPE];
         }
 
-        if (Input.GetKey(KeyCode.RightArrow) && MOVE_NOW == 0 && exchangecount == 0 && STAND == 1)      //移動中でなく右キーが押されたら
+        if (Input.GetKey(KeyCode.D) && MOVE_NOW == 0 && exchangecount == 0 && STAND == 1)      //移動中でなく右キーが押されたら
         {
             MOVE_NOW = 1;
             MOVE_D = 1;
-            STARTPOINT = transform.position.x;                      //誤差修正の為移動開始地点を記録
-            STAND = 0;
-            STAND_T = 0;
-            STAND_U = 0;
+            //STARTPOINT = transform.position.x;                      //誤差修正の為移動開始地点を記録
+            //STAND = 0;
+            //STAND_T = 0;
+            //STAND_U = 0;
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow) && MOVE_NOW == 0 && exchangecount == 0 && STAND == 1)      //移動中でなく左キーが押されたら
+        if (Input.GetKey(KeyCode.A) && MOVE_NOW == 0 && exchangecount == 0 && STAND == 1)      //移動中でなく左キーが押されたら
         {
             MOVE_NOW = 1;
             MOVE_D = -1;
-            STARTPOINT = transform.position.x;                      //誤差修正の為移動開始地点を記録
-            STAND = 0;
-            STAND_T = 0;
-            STAND_U = 0;
+            //STARTPOINT = transform.position.x;                      //誤差修正の為移動開始地点を記録
+            //STAND = 0;
+            //STAND_T = 0;
+            //STAND_U = 0;
         }
 
         if (MOVE_NOW > 0)
         {
             MOVE_NOW--;
 
-            Rigidbody rb = this.GetComponent<Rigidbody>();
-            Vector3 FORCE = new Vector3(10.0f * MOVE_D, 0.0f, 0.0f);
-            //rb.AddForce(FORCE);
+            Vector3 FORCE = new Vector3(20.0f * MOVE_D, 2.0f * MOVE_V, 0.0f);
+            if (rb.velocity.magnitude < 4.0f)
+            {
+                rb.AddForce(FORCE);
+            }
+            //rb.velocity = new Vector3(15.0f * MOVE_D, 0.0f, 0.0f);
 
-            Vector3 pos = transform.position;
-            pos.x += 0.1f * MOVE_D;
-            transform.position = pos;
+            //Vector3 pos = transform.position;
+            //pos.x += 0.1f * MOVE_D;
+            //transform.position = pos;
             if (MOVE_NOW == 0)                                         //移動完了なら
             {
                 //pos.x = STARTPOINT +1 * MOVE_D;
                 //transform.position = pos;                           //誤差を整数加算により補正
             }
-
         }
 
         if (exchangecount > 0)
@@ -151,7 +198,10 @@ public class PLAYER : MonoBehaviour
                     tag = "SOLID";
                     Debug.Log("降下");
                     Physics.gravity = new Vector3(0, -9.8f, 0);
+                    MOVE_V = 1;
                     STAND = 0;
+                    STAND_T = 0;
+                    STAND_U = 0;
                 }
 
                 if (TYPE == 2)
@@ -160,6 +210,9 @@ public class PLAYER : MonoBehaviour
                     Debug.Log("浮上");
                     Physics.gravity = new Vector3(0, 9.8f, 0);
                     STAND = 0;
+                    STAND_T = 0;
+                    STAND_U = 0;
+                    MOVE_V = -1;
                 }
             }
         }
@@ -169,8 +222,8 @@ public class PLAYER : MonoBehaviour
             if (STAND_U == 1)
             {
                 STAND = 1;
-                boxcollider.enabled = true;
-                capcollider.enabled = false;
+                boxcollider.enabled = false;
+                capcollider.enabled = true;
             }
         }
 
@@ -179,8 +232,8 @@ public class PLAYER : MonoBehaviour
             if (STAND_U == 1 || STAND_T == 1)
             {
                 STAND = 1;
-                boxcollider.enabled = true;
-                capcollider.enabled = false;
+                //boxcollider.enabled = true;
+                //capcollider.enabled = false;
             }
         }
 
@@ -189,10 +242,247 @@ public class PLAYER : MonoBehaviour
             if (STAND_T == 1)
             {
                 STAND = 1;
-                boxcollider.enabled = true;
-                capcollider.enabled = false;
+                //boxcollider.enabled = true;
+                //capcollider.enabled = false;
             }
         }
+
+        if (transform.position.x > 0)
+        {
+            posx = (int)transform.position.x;
+        }
+        else
+        {
+            posx = (int)(transform.position.x - 1.0f);
+        }
+
+        if (transform.position.y > 0)
+        {
+            posy = (int)(transform.position.y + 0.5f);
+        }
+        else
+        {
+            posy = (int)(transform.position.y - 0.5f);
+        }
+
+
+        if (posx != B_posx)
+        {
+            if (MOVE_D == 1 && VMOVEflag == 1)  //縦移動予約が入っていて同じ方向に進んでいたら
+            {
+                if (transform.position.x > VMOVEPOS && AUTOMOVEflag == 0)
+                {
+                    //Debug.Log("おちる");
+                    //rb.velocity = new Vector3(2.0f, 0.0f, 0.0f);
+                    STAND = 0;
+                    STAND_T = 0;
+                    STAND_U = 0;
+                    AUTOMOVEflag = 1;
+                }
+            }
+
+            if (MOVE_D == -1 && VMOVEflag == 1)  //縦移動予約が入っていて同じ方向に進んでいたら
+            {
+                if (transform.position.x < VMOVEPOS && AUTOMOVEflag == 0)
+                {
+                    Debug.Log("おちる");
+                    //rb.velocity = new Vector3(2.0f, 0.0f, 0.0f);
+                    STAND = 0;
+                    STAND_T = 0;
+                    STAND_U = 0;
+                    AUTOMOVEflag = 1;
+                }
+            }
+            JUGEMOVE.SETposx(posx);
+            VMOVEflag = JUGEMOVE.root(MOVE_D, MOVE_V);
+            B_MOVE_D = MOVE_D;
+        }
+
+        if (MOVE_D != B_MOVE_D)
+        {
+            VMOVEflag = JUGEMOVE.root(MOVE_D, MOVE_V);
+            B_MOVE_D = MOVE_D;
+        }
+
+        //落下補正用強制移動
+
+        if (AUTOMOVEflag == 1)
+        {
+            if (MOVE_D == 1)
+            {
+                if (transform.position.x < VMOVEPOS + 0.5f)
+                {
+                    rb.velocity = new Vector3(2.0f, 0.0f, 0.0f);
+                }
+
+                if (transform.position.x > VMOVEPOS + 0.5f)
+                {
+                    Vector3 T_pos = transform.position;
+                    T_pos.x = VMOVEPOS + 0.5f;
+                    transform.position = T_pos;
+                    AUTOMOVEflag = 0;
+                    VMOVEflag = 0;
+                    rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+                }
+            }
+
+            if (MOVE_D == -1)
+            {
+                if (transform.position.x > VMOVEPOS - 0.5f)
+                {
+                    rb.velocity = new Vector3(-2.0f, 0.0f, 0.0f);
+                }
+
+                if (transform.position.x < VMOVEPOS - 0.5f)
+                {
+                    Vector3 T_pos = transform.position;
+                    T_pos.x = VMOVEPOS - 0.5f;
+                    transform.position = T_pos;
+                    AUTOMOVEflag = 0;
+                    VMOVEflag = 0;
+                    rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+                }
+            }
+        }
+
+        //ギミック利用よう強制移動
+
+        if (AUTOMOVEflag2 == 1)
+        {
+            if (MOVE_D == 1)
+            {
+                if (transform.position.x < MOVEPOS)
+                {
+                    rb.velocity = new Vector3(1.0f, 0.0f, 0.0f);
+                }
+
+                if (transform.position.x > MOVEPOS)
+                {
+                    Vector3 T_pos = transform.position;
+                    T_pos.x = MOVEPOS;
+                    transform.position = T_pos;
+                    AUTOMOVEflag2 = 0;
+                    MOVEFinish = 1;
+                    rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+                }
+            }
+
+            if (MOVE_D == -1)
+            {
+                if (transform.position.x > MOVEPOS)
+                {
+                    rb.velocity = new Vector3(-1.0f, 0.0f, 0.0f);
+                }
+
+                if (transform.position.x < MOVEPOS)
+                {
+                    Vector3 T_pos = transform.position;
+                    T_pos.x = MOVEPOS;
+                    transform.position = T_pos;
+                    AUTOMOVEflag2 = 0;
+                    MOVEFinish = 1;
+                    rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+                }
+            }
+        }
+
+        if (MOVEFinish == 1)
+        {
+            MOVEFinish = 0;
+
+            //各利用ギミックに対応する処理
+
+            //GAPを使用する場合
+            if (stay_GAP == 1)
+            {
+                capcollider.enabled = false;
+                TARGETV = transform.position.y - 2.1f;
+                GAPMOVE = 1;
+                MOVE_V = -1;
+                //TIME_T = 47;
+            }
+
+            if (stay_HARDHOT == 1)
+            {
+                stay_HARDHOT = 0;
+                rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+                exchangecount = 61;
+                if (TYPE < 2)
+                {
+                    Debug.Log("加熱");
+                    TYPE++;
+                    if (TYPE == 1)
+                    {
+                        SOLID.exchange_s();
+                        AQUA.exchange_b();
+                        tag = "AQUA";
+                    }
+                    if (TYPE == 2)
+                    {
+                        AQUA.exchange_s();
+                        CLOUD.exchange_b();
+                        /*
+                        tag = "CLOUD";
+                        Debug.Log("浮上");
+                        Physics.gravity = new Vector3(0, 9.8f, 0);
+                        */
+                    }
+                }
+            }
+
+            if (stay_HARDCOLD == 1)
+            {
+                stay_HARDCOLD = 0;
+                rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+                exchangecount = 61;
+                if (TYPE > 0)
+                {
+                    Debug.Log("冷却");
+                    TYPE--;
+                    if (TYPE == 0)
+                    {
+                        AQUA.exchange_s();
+                        SOLID.exchange_b();
+                        /*
+                        tag = "SOLID";
+                        Debug.Log("降下");
+                        Physics.gravity = new Vector3(0, -9.8f, 0);
+                        */
+                    }
+                    if (TYPE == 1)
+                    {
+                        CLOUD.exchange_s();
+                        AQUA.exchange_b();
+                        tag = "AQUA";
+                    }
+                }
+            }
+        }
+
+        if (GAPMOVE == 1)
+        {
+            //TIME_T--;
+            if (transform.position.y < TARGETV)
+            {
+                capcollider.enabled = true;
+
+                Debug.Log("浮上");
+                rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+                Physics.gravity = new Vector3(0, 9.8f, 0);
+            }
+        }
+
+        if (posy != B_posy)
+        {
+            VMOVEflag = 0;
+            JUGEMOVE.SETposy(posy);
+            VMOVEflag = JUGEMOVE.root(MOVE_D, MOVE_V);
+        }
+
+        B_posx = posx;
+        B_posy = posy;
+        //Vector3 pos_p = transform.position;
+        //PLAYERCAMERA.SETPOS(pos_p);
     }
 
     //接地（面）関係　（追加予定）
@@ -225,7 +515,7 @@ public class PLAYER : MonoBehaviour
         }
     }
 
-    public void CLEAR_stayHOT()
+    public void CLEAR_stay_HOT()
     {
         stay_HOT = 0;
     }
@@ -249,7 +539,12 @@ public class PLAYER : MonoBehaviour
         }
     }
 
-    public void CLEAR_stayCOLD()
+    public void SET_stay_COLD()
+    {
+        stay_COLD = 1;
+    }
+
+    public void CLEAR_stay_COLD()
     {
         stay_COLD = 0;
     }
@@ -270,11 +565,79 @@ public class PLAYER : MonoBehaviour
         }
     }
 
-    public void CLARE_STAND()
+    public void CLEAR_STAND()
     {
         STAND = 0;
         boxcollider.enabled = false;
         capcollider.enabled = true;
     }
-}
 
+    public Vector3 GETPLAYERPOS()
+    {
+        Vector3 pos = transform.position;
+        return pos;
+    }
+
+    public void SET_V_MOVEPOS(float x)
+    {
+        if (VMOVEflag == 0)
+        {
+            VMOVEPOS = x - 0.5f * MOVE_D;
+            //VMOVEPOS = x;
+        }
+        //Debug.Log(transform.position);
+    }
+
+    public void SET_stay_GAP()
+    {
+        stay_GAP = 1;
+    }
+
+    public void CLEAR_stay_GAP()
+    {
+        stay_GAP = 0;
+    }
+
+    public void SET_stay_HOT()
+    {
+        stay_HOT = 1;
+    }
+
+    public void HARDHOT()
+    {
+        //capcollider.enabled = false;
+        Debug.Log(transform.position);
+        MOVEPOS = JUGEMOVE.GET_JUGEPOS();   //目標地点Xを判定マスと同じにする
+
+        if (transform.position.x > MOVEPOS)
+        {
+            MOVE_D = -1;
+        }
+        else
+        {
+            MOVE_D = 1;
+        }
+
+        AUTOMOVEflag2 = 1;
+        stay_HARDHOT = 1;
+    }
+
+    public void HARDCOLD()
+    {
+        //capcollider.enabled = false;
+        Debug.Log(transform.position);
+        MOVEPOS = JUGEMOVE.GET_JUGEPOS();   //目標地点Xを判定マスと同じにする
+
+        if (transform.position.x > MOVEPOS)
+        {
+            MOVE_D = -1;
+        }
+        else
+        {
+            MOVE_D = 1;
+        }
+
+        AUTOMOVEflag2 = 1;
+        stay_HARDCOLD = 1;
+    }
+}
