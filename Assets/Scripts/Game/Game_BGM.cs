0e4523cb70dca59696a4ee46a5c26cbe67aee705
komachi114 <oreoresuke@gmail.com>
2020-05-29 16:34:13 +0900

    BGM追加

--- Assets/Scripts/Game/Game_BGM.cs
+++ Assets/Scripts/Game/Game_BGM.cs
@@ -7,6 +7,7 @@ public class Game_BGM : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        FindObjectOfType<Audio_Manager>().Stop("Result");
         var index = DontDestroyManager.Map_Index / 10 + 1;
         var bgm_name = "world";
         bgm_name = bgm_name + index.ToString();
--- Assets/Scripts/Game/MapLoader.cs
+++ Assets/Scripts/Game/MapLoader.cs
@@ -97,7 +97,7 @@ public class MapLoader : MonoBehaviour
             m_map_datas.Add(data);
         }
 
-        m_map_index = 31;
+        m_map_index = 0;
         //Debug.Log(m_map_index);
         //  指定したインデックスのマップを生成する
         Map_Create(m_map_datas[m_map_index]);
