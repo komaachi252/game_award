611458b9f1749e5c78f2fb39a5a94e1c5b4594a4
hakiyoshi <harusyu0306@gmail.com>
2020-06-07 16:17:41 +0900

    救出フラグ貰う処理作った

---
+++ Assets/Scripts/Game_UI/Expose/ExposeIsHelp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Expose Scene Property/ExposeIsHelp", fileName = "ExposeIsHelp")]
+
+public class ExposeIsHelp : ScriptableObject
+{
+
+    public Helpme Expose_Helpme { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
