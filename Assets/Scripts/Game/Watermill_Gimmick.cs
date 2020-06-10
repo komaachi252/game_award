51c7db98d406feaf21f2c90a0656aec46d9db87a
komachi114 <oreoresuke@gmail.com>
2020-06-10 10:16:46 +0900

    水門上方修正

--- Assets/Scripts/Game/Watermill_Gimmick.cs
+++ Assets/Scripts/Game/Watermill_Gimmick.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Watermill_Gimmick : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class Watermill_Gimmick : MonoBehaviour
     const float MAX_DISTANCE = 4.0f;
     float m_total_dist = 0.0f;
     bool m_is_move;  //  動かされているか？
+    bool m_is_trigger;  //  動かされているか？
+
+    float m_pos_y;
     //bool m_is_max_dist = false;
     enum Gate_Phase
     {
@@ -22,6 +26,7 @@ public class Watermill_Gimmick : MonoBehaviour
     {
         this.gameObject.GetComponent<Transform>().transform.Translate(0, -0.5f, 0);
         m_is_move = false;
+        m_is_trigger = false;
     }
 
     // Update is called once per frame
@@ -50,6 +55,11 @@ public class Watermill_Gimmick : MonoBehaviour
         {
             m_is_move = true;
         }
+
+        if(!m_is_trigger && Mathf.Abs(move_dist) == 0.0f && m_is_move)
+        {
+            m_is_trigger = true;
+        }
         
         if (move_dist > 0.0f && m_trigger_up.GetComponent<WaterGate_Trigger>().Is_Colli)
         {
@@ -61,18 +71,39 @@ public class Watermill_Gimmick : MonoBehaviour
             Debug.Log(move_dist + "down");
             return;
         }
-
-        Debug.Log(move_dist);
-        if(move_dist == 0.0f)
+        
+        if(m_is_trigger && m_is_move)
         {
-            var pos_y = Mathf.CeilToInt(this.gameObject.transform.position.y + 0.5f);
-            Debug.Log(pos_y);
-            if (pos_y > this.gameObject.transform.position.y + 0.5f)
+            //m_pos_y = Mathf.Round(this.gameObject.transform.position.y + 0.5f);
+            m_pos_y = (float)Math.Round(this.gameObject.transform.position.y + 0.5f, MidpointRounding.AwayFromZero);
+           // m_is_trigger = false;
+            m_is_move = false;
+        }
+
+        if (move_dist == 0.0f  && m_is_trigger)
+        {
+            if (m_pos_y > this.gameObject.transform.position.y + 0.5f)
             {
                 move_dist += 0.01f;
+                if (m_pos_y <= this.gameObject.transform.position.y + 0.5f + move_dist)
+                {
+                    m_is_trigger = false;
+                    var pos = this.gameObject.transform.position;
+                    pos.y = m_pos_y;
+                }
             }
-
+            if (m_pos_y < this.gameObject.transform.position.y + 0.5f)
+            {
+                move_dist -= 0.01f;
+                if (m_pos_y >= this.gameObject.transform.position.y + 0.5f + move_dist)
+                {
+                    m_is_trigger = false;
+                    var pos = this.gameObject.transform.position;
+                    pos.y = m_pos_y;
+                }
+            }
         }
+
 
         this.transform.Translate(0.0f, move_dist, 0.0f);
 
