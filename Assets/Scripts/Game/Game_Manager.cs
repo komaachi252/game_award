3dd16492b83e213baa26274424ca94861b018be2
NABRL <h_yu_nabrl@yahoo.co.jp>
2020-06-12 23:03:18 +0900

    水車の音の修正、デバックコマンドを自分が認識している分Ｇキー、Ｒキーを封印

--- Assets/Scripts/Game/Game_Manager.cs
+++ Assets/Scripts/Game/Game_Manager.cs
@@ -23,10 +23,12 @@ public class Game_Manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         if(Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene("GameScene");
         }
+        */
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             UnityEngine.Application.Quit();
