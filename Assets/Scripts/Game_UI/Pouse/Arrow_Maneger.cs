d69fece02b807400e6f6bd3369ae05ad8a8836b4
hakiyoshi <harusyu0306@gmail.com>
2020-06-13 20:50:55 +0900

    即席めんもびっくり速攻作った案内板

---
+++ Assets/Scripts/Game_UI/Pouse/Arrow_Maneger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow_Maneger : MonoBehaviour
+{
+
+    [SerializeField] Animator anime01;
+    [SerializeField] Animator anime02;
+
+    [SerializeField] ExposeNowMode now_mode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (StageController.Get_Index() == 2)
+        {
+            GameObject now = now_mode.expose_now_mode.Get_NowMode;//今のモードを受け取る
+
+            if (now.name == "water")//水
+            {
+                anime01.SetBool("Start", true);
+                anime01.SetBool("No", false);
+
+                anime02.SetBool("Start", true);
+                anime02.SetBool("No", false);
+            }
+            else
+            {
+                anime01.SetBool("Start", false);
+                anime01.SetBool("No", true);
+
+                anime02.SetBool("Start", false);
+                anime02.SetBool("No", true);
+            }
+        }
+
+       
+    }
+}
